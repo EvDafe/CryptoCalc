6eb50a81bc7a6caf25af9db83a532a9c81cd2ffa
SnappyNVM <112257868+SnappyNVM@users.noreply.github.com>
2024-07-04 20:22:48 +0500

    +Buttons

--- Assets/Scripts/AppInstasller.cs
+++ Assets/Scripts/AppInstasller.cs
@@ -4,9 +4,11 @@ using Zenject;
 public class AppInstasller : MonoInstaller
 {
     [SerializeField] private ThemeChangePoint _themeChangePoint;
+    [SerializeField] private LanguagesContainer _languagesContainer;
 
     public override void InstallBindings()
     {
         Container.Bind<ThemeChangePoint>().FromInstance(_themeChangePoint).AsSingle().Lazy();
+        Container.Bind<LanguagesContainer>().FromInstance(_languagesContainer).AsSingle().NonLazy();
     }
 }
